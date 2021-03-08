a0361821eedca456043b53e134ecc75b0266b93f
CYZ <1822550890@qq.com>
2021-03-08 22:02:16 +0000

    部署servlet包

--- JavaWeb/07_Servlet/src/test/HelloServlet.java
+++ JavaWeb/07_Servlet/src/test/HelloServlet.java
@@ -1,8 +1,37 @@
 package test;
 
+import javax.servlet.*;
+import java.io.IOException;
+
 /**
  * @author Chenyang
  * @create 2021-03-08-21:55
  */
-public class HelloServlet {
+public class HelloServlet implements Servlet {
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    //
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
 }
