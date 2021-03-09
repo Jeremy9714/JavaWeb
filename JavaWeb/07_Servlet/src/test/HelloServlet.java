2ac2a6f799352a919a37855f44fdd8961766f015
CYZ <1822550890@qq.com>
2021-03-09 12:48:03 +0000

    请求的分发处理(获取请求方式并执行不同代码)

--- JavaWeb/07_Servlet/src/test/HelloServlet.java
+++ JavaWeb/07_Servlet/src/test/HelloServlet.java
@@ -1,6 +1,8 @@
 package test;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 /**
@@ -27,6 +29,24 @@ public class HelloServlet implements Servlet {
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
         System.out.println("HelloServlet被访问了");
+        //向下强转为子类以使用getMethod()方法
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        //getMethod()方法用于获取请求的方式(GET、POST)
+        String method = httpServletRequest.getMethod();
+        //根据请求方式的不同，执行不同的操作
+        if ("GET".equals(method)) {
+            doGet();
+        } else if ("POST".equals(method)) {
+            doPost();
+        }
+    }
+    //get请求的操作
+    public void doGet(){
+        System.out.println("get操作");
+    }
+    //post请求的操作
+    public void doPost(){
+        System.out.println("post操作");
     }
 
     @Override
