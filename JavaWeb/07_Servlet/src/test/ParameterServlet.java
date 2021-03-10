0f290a9ca2425761a7222f00484f3712d1e9b9b0
CYZ <1822550890@qq.com>
2021-03-10 12:19:21 +0000

    HttpServletRequest类常用方法(获取请求信息)

--- JavaWeb/07_Servlet/src/test/ParameterServlet.java
+++ JavaWeb/07_Servlet/src/test/ParameterServlet.java
@@ -13,6 +13,7 @@ import java.util.Arrays;
 public class ParameterServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("-----------doget------------");
         //getParameter(String): 获取请求的参数
         String username = req.getParameter("username");
         String password = req.getParameter("password");
@@ -24,7 +25,17 @@ public class ParameterServlet extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //设置请求体的字符集为UTF-8,解决post请求的中文乱码问题
+        //此方法在获取请求参数前调用才有效
+        req.setCharacterEncoding("UTF-8");
+        System.out.println("-----------dopost------------");
+        //获取请求参数
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        String[] hobbies = req.getParameterValues("hobby");
+        System.out.println("用户名: " + username);
+        System.out.println("密码: " + password);
+        System.out.println("爱好: " + Arrays.asList(hobbies));
     }
 }
