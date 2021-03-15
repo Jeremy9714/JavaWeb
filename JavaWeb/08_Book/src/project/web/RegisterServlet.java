6023f3d6ec3d8d2242fab78193f2a0844b08277a
CYZ <1822550890@qq.com>
2021-03-15 16:02:28 +0000

    配置

---
+++ JavaWeb/08_Book/src/project/web/RegisterServlet.java
@@ -0,0 +1,18 @@
+package project.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Chenyang
+ * @create 2021-03-15-15:30
+ */
+public class RegisterServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
