20dc5bde190c3bd836bae08b34320013b6d18ffc
CYZ <1822550890@qq.com>
2021-03-20 12:45:15 +0000

    文件的下载(解决在使用URL编码的浏览器中下载包含中文名文件时出现乱码情况)

--- JavaWeb/10_EL_JSTL/src/servlet/DownloadServlet.java
+++ JavaWeb/10_EL_JSTL/src/servlet/DownloadServlet.java
@@ -10,9 +10,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URLEncoder;
 
 /**
  * 文件下载
+ *
  * @author Chenyang
  * @create 2021-03-20-11:42
  */
@@ -29,15 +31,19 @@ public class DownloadServlet extends HttpServlet {
         //3.回传前，通过响应头告诉客户端返回的数据类型
         //获取要下载的文件的类型
         String mimeType = servletContext.getMimeType("/file/" + fileName);
+        System.out.println("下载的文件类型为: " + mimeType);
         resp.setContentType(mimeType);
 
         //4.还要通过响应头告诉客户端，收到的数据是用于下载使用
         /*
-        * Content-Disposition响应头设置收到的数据的处理方式
-        * attachment表示用于下载的附件
-        * filename表示下载的文件名
-        * */
-        resp.setHeader("Content-Disposition","attachment; filename=" + fileName);
+         * Content-Disposition响应头设置收到的数据的处理方式
+         * attachment表示用于下载的附件
+         * filename表示下载的文件名
+         * 若下载名包含中文，则需要对其进行url编码
+         * */
+        //URLEncoder.encode(name,charset)对字符串进行url编码
+        resp.setHeader("Content-Disposition", "attachment; filename="
+                + URLEncoder.encode("中文名.jpg", "UTF-8"));
 
         //5.把要下载的内容回传给客户端
         //以输入流的形式获取要下载的内容
@@ -45,6 +51,6 @@ public class DownloadServlet extends HttpServlet {
         //设置响应流
         ServletOutputStream os = resp.getOutputStream();
         //通过commons-io.jar包的IOUtils工具类方法将输入流内容复制到输出流
-        IOUtils.copy(is,os);
+        IOUtils.copy(is, os);
     }
 }
