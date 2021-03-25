eaafad8dfec64615563376bd1f6528e4748f9745
CYZ <1822550890@qq.com>
2021-03-25 10:13:12 +0000

    FilterConfig对象的作用(获取过滤器别名、获取web.xml文件中的初始化参数、获取ServletContext对象)

--- JavaWeb/13_Filter/src/filter/AdminFilter.java
+++ JavaWeb/13_Filter/src/filter/AdminFilter.java
@@ -10,14 +10,28 @@ import java.io.IOException;
  * @create 2021-03-24-21:07
  */
 public class AdminFilter implements Filter {
+
+    public AdminFilter() {
+        System.out.println("1.Filter的构造器方法");
+    }
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("2.Filter的ini初始化t方法");
 
+        //获取Filter过滤器的别名
+        System.out.println("过滤器名称: " + filterConfig.getFilterName());
+        //获取web.xml配置文件中的初始化参数
+        System.out.println("uername: " + filterConfig.getInitParameter("username"));
+        System.out.println("url: " + filterConfig.getInitParameter("url"));
+        //获取ServletContext对象
+        System.out.println("ServletContext对象: " + filterConfig.getServletContext());
     }
 
     //专门用于拦截请求，可以做权限检查
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("3.Filter的doFilter()过滤方法");
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpSession session = request.getSession();
         Object user = session.getAttribute("user");
@@ -34,6 +48,6 @@ public class AdminFilter implements Filter {
 
     @Override
     public void destroy() {
-
+        System.out.println("4.Filter的destroy销毁方法");
     }
 }
