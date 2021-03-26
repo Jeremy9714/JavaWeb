7b875a0936e29a92bc6226ef41dc42044cfce0b2
CYZ <1822550890@qq.com>
2021-03-26 17:06:54 +0000

    List、Map与json的相互转换(toJson()、fromJson()及TypeToken匿名类对象的使用)

--- JavaWeb/14_json_ajax_i18n/src/json/JsonTest.java
+++ JavaWeb/14_json_ajax_i18n/src/json/JsonTest.java
@@ -2,7 +2,13 @@ package json;
 
 import bean.Person;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Chenyang
@@ -27,9 +33,43 @@ public class JsonTest {
     }
 
     //List与json的互相转换
+    @Test
+    public void test2() {
+        List<Person> personList = new ArrayList<>();
+        personList.add(new Person(1, "person_1"));
+        personList.add(new Person(2, "person_2"));
+        personList.add(new Person(3, "person_3"));
 
+        Gson gson = new Gson();
+        //将List转换为json字符串
+        String personListJsonStr = gson.toJson(personList);
+        System.out.println(personListJsonStr);
+
+        //将json字符串转换为List
+        //需要创建一个类继承TypeToken，并设置一个泛型类型(要转换的List类型)
+        //实例化该类，并通过getType()方法将返回的Type对象传入fromJson方法中
+        List<Person> personlist1 = gson.fromJson(personListJsonStr, new PersonListType().getType());
+        System.out.println(personlist1);
+    }
 
     //Map与json的互相转换
+    @Test
+    public void test3() {
+        Map<Integer, Person> personMap = new HashMap<>();
+        personMap.put(1, new Person(1, "Person_1"));
+        personMap.put(2, new Person(2, "Person_2"));
+        personMap.put(3, new Person(3, "Person_3"));
 
+        Gson gson = new Gson();
+        //将Map转换为json字符串
+        String personMapJsonStr = gson.toJson(personMap);
+        System.out.println(personMapJsonStr);
+
+        //将json字符串转换为Map
+        //使用匿名类对象
+        Map<Integer, Person> personMap1 = gson.fromJson(personMapJsonStr,
+                new TypeToken<Map<Integer, Person>>() {}.getType());
+        System.out.println(personMap1);
+    }
 
 }
---
+++ JavaWeb/14_json_ajax_i18n/src/json/PersonListType.java
@@ -0,0 +1,13 @@
+package json;
+
+import bean.Person;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.List;
+
+/**
+ * @author Chenyang
+ * @create 2021-03-26-16:46
+ */
+public class PersonListType extends TypeToken<List<Person>> {
+}
